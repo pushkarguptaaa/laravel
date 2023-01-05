0f1f1101f094c72fdff8e9f92fcac8fad5cb59d3
pushkarguptaaa <pushkardps8@gmail.com>
2023-01-05 12:15:22 +0530

    created a basic model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('listings',[
         'heading' => 'Latest Listings',
-        'listings' => [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis at ea nobis quos facilis magni voluptate quia aliquam natus, vitae inventore velit dicta! Vitae nemo assumenda obcaecati quia laborum.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis at ea nobis quos facilis magni voluptate quia aliquam natus, vitae inventore velit dicta! Vitae nemo assumenda obcaecati quia laborum.'
-            ]
-        ]
+        'listings' => Listing::all()
     ]);
+});
+
+Route::get('/listings/{id}',function($id){
+   return view('listing',[
+    'listing' => Listing::find($id)
+   ]); 
 });
