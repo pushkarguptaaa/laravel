56689043eee649bf3b3653ff14385ce371cc1f4f
pushkarguptaaa <pushkardps8@gmail.com>
2023-01-31 13:37:55 +0530

    Route model binding

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/listings/{id}',function($id){
-   return view('listing',[
-    'listing' => Listing::find($id)
-   ]); 
+Route::get('/listings/{listing}',function(Listing $listing){
+    return view('listing',[
+        'listing' => $listing
+       ]);
+    
 });
