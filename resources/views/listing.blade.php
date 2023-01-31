aa00f20ae9aa4cd5397ade774914d02bc474cd56
pushkarguptaaa <pushkardps8@gmail.com>
2023-01-31 15:27:37 +0530

    blade components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
                 ><i class="fa-solid fa-arrow-left"></i> Back
             </a>
             <div class="mx-4">
-                <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+                <x-card>
                     <div
                         class="flex flex-col items-center justify-center text-center"
                     >
@@ -69,6 +69,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </x-card>
             </div>
 @endsection
