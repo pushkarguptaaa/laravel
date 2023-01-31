a3182a9fab76a9b3a377eaf155917c58e90e5338
pushkarguptaaa <pushkardps8@gmail.com>
2023-01-31 16:55:24 +0530

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -14,17 +15,8 @@ use App\Models\Listing;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/', function () {
-    return view('listings',[
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
-
-Route::get('/listings/{listing}',function(Listing $listing){
-    return view('listing',[
-        'listing' => $listing
-       ]);
-    
-});
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
