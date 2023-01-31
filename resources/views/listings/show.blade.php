6eb8a6deb811e889d45f92e870b7472ab2402a7f
pushkarguptaaa <pushkardps8@gmail.com>
2023-01-31 17:52:59 +0530

    Using layout component

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,6 +1,5 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
 @include('partials._search')
 
 <a href="/" class="inline-block text-black ml-4 mb-4"
@@ -52,4 +51,4 @@
                     </div>
                 </x-card>
             </div>
-@endsection
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ use App\Models\Listing;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing 
+
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
