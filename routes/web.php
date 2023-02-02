805fe85f908f5754f169f8eeb8369c25840aa643
pushkarguptaaa <pushkardps8@gmail.com>
2023-02-02 10:06:38 +0530

    User Login

--- routes/web.php
+++ routes/web.php
@@ -57,4 +57,10 @@ Route::post('/users',[UserController::class,'store']);
 // Log User Out
 Route::post('/logout',[UserController::class,'logout']);
 
+// Show Login Form
+Route::get('/login',[UserController::class,'login']);
+
+// Login User
+Route::post('/users/authenticate',[UserController::class,'authenticate']);
+
 
