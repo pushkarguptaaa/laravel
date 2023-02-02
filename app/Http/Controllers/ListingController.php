f33587f9b6647645f12244d3b6cc61bf84132078
pushkarguptaaa <pushkardps8@gmail.com>
2023-02-02 13:21:09 +0530

    Added ownership to listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -43,6 +43,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos','public');
         }
 
+        $formFields['user_id'] = auth()->id();
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
@@ -69,7 +71,7 @@ class ListingController extends Controller
 
         if($request->hasFile('logo')){
             $formFields['logo'] = $request->file('logo')->store('logos','public');
-        }
+        } 
 
         $listing->update($formFields);
 
